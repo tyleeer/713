7e9f2f85ccecb143691aa7a39ab8196de0fc5cfe
tyleeer <siradanai.nam@gmail.com>
2025-02-02 23:55:29 +0700

    add(lab01): add task 10

---
+++ 713-Lab01/callFunction.js
@@ -0,0 +1,2 @@
+const { multiply } = require("./function");
+console.log(multiply(2, 3));
---
+++ 713-Lab01/function.js
@@ -0,0 +1,7 @@
+const multiply = (x, y) => {
+  return x * y;
+};
+console.log(multiply(5, 10));
+module.exports = {
+  multiply,
+};
