c405a4ab9ee8f86fcc955d49ba230328198a0bb0
tyleeer <siradanai.nam@gmail.com>
2025-02-08 14:20:53 +0700

    update(lab02): update task 4
    refactor to make code cleaner

--- 713-Lab02/src/function.ts
+++ 713-Lab02/src/function.ts
@@ -2,14 +2,20 @@ const add = (a: number, b: number): string => {
   const result = a + b;
   return result.toString();
 };
-export default add;
+// export default add;
 
-export const subtract = (a: number, b: number): string => {
+const subtract = (a: number, b: number): string => {
   const result = a - b;
   return result.toString();
 };
 
-export const findMax = (numberList: number[]): string => {
+const findMax = (numberList: number[]): string => {
   if (numberList.length <= 1) return "Please provide at least 2 numbers.";
   return Math.max(...numberList).toString();
+};
+
+export default {
+  add,
+  subtract,
+  findMax,
 };
--- 713-Lab02/src/index.ts
+++ 713-Lab02/src/index.ts
@@ -1,8 +1,8 @@
-import add, { subtract, findMax } from "./function";
-const result = add(1, 2) + 0;
-const result2 = subtract(1, 2) + 0;
+import operations from "./function";
+const result = operations.add(1, 2) + 0;
+const result2 = operations.subtract(1, 2) + 0;
 console.log(result, "type of result:", typeof result);
 console.log(result2, "type of result2:", typeof result2);
 
-const result3 = findMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100]);
+const result3 = operations.findMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100]);
 console.log(result3, "type of result2:", typeof result2);
