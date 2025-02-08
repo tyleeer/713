82c84d74bc6f21e21349770c085ec4925f31c049
tyleeer <siradanai.nam@gmail.com>
2025-02-08 14:08:10 +0700

    add(lab02): add task 4
    import export function

--- 713-Lab02/src/index.ts
+++ 713-Lab02/src/index.ts
@@ -1,15 +1,8 @@
-const add = (a: number, b: number): string => {
-  const result = a + b;
-  return result.toString();
-};
-
+import add, { subtract, findMax } from "./function";
 const result = add(1, 2) + 0;
+const result2 = subtract(1, 2) + 0;
 console.log(result, "type of result:", typeof result);
-
-const findMax = (numberList: number[]): string => {
-  if (numberList.length <= 1) return "Please provide at least 2 numbers.";
-  return Math.max(...numberList).toString();
-};
-
-const result2 = findMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100]);
 console.log(result2, "type of result2:", typeof result2);
+
+const result3 = findMax([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100]);
+console.log(result3, "type of result2:", typeof result2);
