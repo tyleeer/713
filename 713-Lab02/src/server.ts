8a4314c185b12f6d36167a05a455a0444d5f5705
tyleeer <siradanai.nam@gmail.com>
2025-02-12 23:07:14 +0700

    add(lab02): add task 10
    create endpoint to query data

--- 713-Lab02/src/server.ts
+++ 713-Lab02/src/server.ts
@@ -2,6 +2,87 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 3000;
 
+interface Event {
+  id: number;
+  category: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  time: string;
+  petsAllowed: boolean;
+  organizer: string;
+}
+
+const events: Event[] = [
+  {
+    id: 1,
+    category: "Music",
+    title: "Concert",
+    description: "A live concert",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 2,
+    category: "Sports",
+    title: "Marathon",
+    description: "A world class marathon",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 3,
+    category: "Books",
+    title: "Scientific books",
+    description: "A scientific book",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 4,
+    category: "Job fair",
+    title: "New job oppotunities",
+    description: "A job fair",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 5,
+    category: "Music",
+    title: "EDM Concert",
+    description: "A live EDM concert",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+  {
+    id: 6,
+    category: "Music",
+    title: "Indie Concert",
+    description: "A live Indie concert",
+    location: "London",
+    date: "2021-07-01",
+    time: "19:00",
+    petsAllowed: false,
+    organizer: "Live Nation",
+  },
+];
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
@@ -19,6 +100,18 @@ app.get("/test/param", (req: Request, res: Response) => {
   const id = req.query.id;
   const output = `id: ${id}`;
   res.send(output);
+});
+
+app.get("/events", (req, res) => {
+  if (req.query.category) {
+    const category = req.query.category;
+    const filteredEvents = events.filter(
+      (event) => event.category === category
+    );
+    res.json(filteredEvents);
+  } else {
+    res.json(events);
+  }
 });
 
 app.listen(port, () => {
